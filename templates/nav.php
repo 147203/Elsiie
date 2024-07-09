af77aec4bbffa6d25f24a0d4be5c0dd37f5cc0ab
147203 <margaret.mathai@strathmore.edu>
2024-07-09 21:17:52 +0300

    created code reusability

---
+++ templates/nav.php
@@ -0,0 +1,14 @@
+<nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="services.php">Services</a></li>
+                <li><a href="contacts.php">Contact</a></li>
+                <li><a href="review.php">Feedback</a></li>
+                <li><a href="products.php">Products</a></li>
+                <li><a href="view_messages.php">message</a></li>
+                
+
+
+            </ul>
+        </nav>
